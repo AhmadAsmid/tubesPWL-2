dcec777790bedd4597ae790e9c16d81bdce3c24c
Ahmad Asmid <Ahmadnaza167@gmail.com>
2024-12-16 21:02:08 +0700

    menambahkan table transaksi

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('/admin/cabang', AdminController::class);
+});
+Route::middleware(['auth', 'role:manajer'])->group(function () {
+    Route::get('/manajer/laporan', [ManajerController::class, 'laporan']);
+});
